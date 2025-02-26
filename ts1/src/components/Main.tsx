ab0e2f40c3766da77469ecd1bacc0c95ab64cd27
emarishkin <ee.marishkin@yandex.ru>
2025-02-26 21:53:01 +0400

    six map

--- ts1/src/components/Main.tsx
+++ ts1/src/components/Main.tsx
@@ -6,14 +6,18 @@ import { variants } from "../data/variants";
 export function Main(){
     return(
         <div className="main">
-           <Product product={products[0]} />
+           {products.map(product=><Product product={product} />)}
+           {variants.map(variant=><Variant variant={variant} />)}
+           
+           
+           {/* <Product product={products[0]} />
            <Product product={products[1]} />
            <Product product={products[2]} />
            <Product product={products[2]} />
            <Product product={products[2]} />
            <Variant variant={variants[0]} />
            <Variant variant={variants[1]} />
-           <Variant variant={variants[2]} />
+           <Variant variant={variants[2]} /> */}
         </div>
     )
 }
