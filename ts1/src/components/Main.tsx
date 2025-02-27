be991af7734e7fe696af6e7367e764df50e38a1c
emarishkin <ee.marishkin@yandex.ru>
2025-02-27 12:42:56 +0400

    eight break modal

--- ts1/src/components/Main.tsx
+++ ts1/src/components/Main.tsx
@@ -1,34 +1,37 @@
 import { Product } from "./Product";
-// import { products } from "../data/products";
+import { products } from "../data/products";
 import { Variant } from "./Variant";
 import { variants } from "../data/variants";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { IProduct } from "../models";
+import { ModalWindow } from "./ModalWindow";
 
 
 
 
 export function Main(){
 
-const [products,setProducts] = useState<IProduct[]>([])
+// const [products,setProducts] = useState<IProduct[]>([])
 
-async function fetchProducts() {
-   const response = await axios.get<IProduct[]>('https://fakestoreapi.in/api/products?limit=5')
-   setProducts(response.data)
-}
+// async function fetchProducts() {
+//    const response = await axios.get<IProduct[]>('https://fakestoreapi.in/api/products?limit=5')
+//    setProducts(response.data)
+// }
 
 
-useEffect(()=>{
-    fetchProducts()
-},[])
+// useEffect(()=>{
+//     fetchProducts()
+// },[])
 
 
     return(
         <div className="main">
            {products.map(product=><Product product={product} key={product.id} />)}
            {variants.map(variant=><Variant variant={variant} key={variant.id}/>)}
-           
+           <div>
+            <ModalWindow/>
+           </div>
            {/* <Product product={products[0]} />
            <Product product={products[1]} />
            <Product product={products[2]} />
---
+++ ts1/src/components/ModalWindow.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export function ModalWindow(){
+
+const [del,setDel]= useState(false)
+
+    return(
+        <div className="modalWindow">
+         <button onClick={()=>setDel(true)}>close</button>
+         {!del && <>1</>}
+         {del && <p>rrr</p>}
+        </div>
+
+       
+    )
+}
