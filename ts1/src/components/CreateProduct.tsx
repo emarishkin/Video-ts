01b8ac95d56aa13bf8fca7915952662e3fe1eebd
emarishkin <ee.marishkin@yandex.ru>
2025-03-01 21:14:33 +0400

    11 server

--- ts1/src/components/CreateProduct.tsx
+++ ts1/src/components/CreateProduct.tsx
@@ -1,13 +1,33 @@
 import { useState } from "react"
+import { IProduct } from "../models"
+import axios from "axios"
+
+
+const productData:IProduct = {
+    id:5,
+    title:'',
+    image:'',
+    price:11,
+    description:'r'
+  }
+
+
+
+
+
+
 
 export function CreateProducts(){
-
 
 const [value,setValue]= useState('')
 
 
-const submitHandler = (event:React.FormEvent)=>{
+const submitHandler = async (event:React.FormEvent)=>{
     event.preventDefault()
+
+productData.title= value
+
+  await axios.post<IProduct>('https://fakestoreapi.in/api/products',productData)
 }
 
 // const changeHandle = (event:React.KeyboardEvent<HTMLInputElement>) => {
